40699988b7f353eb6216e913455af07a870eb44e
Thales Sinelli Lima <thalessinelli@gmail.com>
2022-08-21 18:22:31 -0300

    Cognito setup

---
+++ SharedResources/output.tf
@@ -0,0 +1,20 @@
+output "cognito_arn_dev"{
+    value = aws_cognito_user_pool.image_clustering_user_pool_dev.arn
+}
+
+output "user_pool_id" {
+  description = "name of the user pool"
+  value       = aws_cognito_user_pool.image_clustering_user_pool_dev.id
+}
+
+
+output "client_id" {
+  description = "The ids of the user pool clients"
+  value       = aws_cognito_user_pool_client.client.id 
+}
+
+output "client_secret" {
+  description = " The client secrets of the user pool clients"
+  value       = aws_cognito_user_pool_client.client.client_secret
+  sensitive   = true
+}
